318e7939b0fedc6f9f42de61d62263201e696fb1
Auliya Michelle <48114085+Auliya123@users.noreply.github.com>
2021-04-23 15:45:14 +0700

    use context and use reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React, { useReducer } from "react";
 import "./styles.css";
+import ComponentA from "./ComponentA";
+
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 export default function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className="App">
+        Count {count}
+        <ComponentA />
+      </div>
+    </CountContext.Provider>
   );
 }
